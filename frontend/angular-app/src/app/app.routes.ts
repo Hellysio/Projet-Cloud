9e54126be8430a8cbea23e41dbd7271026763acd
Hellysio <eliotasura@gmail.com>
2024-10-28 18:22:11 +0100

    Changed files

--- frontend/angular-app/src/app/app.routes.ts
+++ frontend/angular-app/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { FeaturesComponent } from './features/features.component';
 import { LandingComponent } from './landing/landing.component';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
     { path: 'features', component: FeaturesComponent },
     { path: '', component: LandingComponent },
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+
+export class AppRoutingModule {}
