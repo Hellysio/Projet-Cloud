7681ec500c57fb1ae32f92442e97f12c468fe88b
Hellysio <eliotasura@gmail.com>
2024-11-10 15:53:22 +0100

    Made changes on dockerfiles and compose file

--- backend/myproject/api/views.py
+++ backend/myproject/api/views.py
@@ -1,21 +1,13 @@
 from django.utils import timezone 
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status, viewsets
-from django.core.files.storage import default_storage, FileSystemStorage
-from dotenv import load_dotenv
+from rest_framework import status
 from .models import UploadedFiles
 import requests
 
-## Maybe consider consider using a dot env file to store the API key and all the db info
 @api_view(['POST'])
 def upload_image(request):
-    # print('Request:', request)
-    # print('Request data:', request.data)
-    # print('list', request.FILES.getlist('fileUpload'))
-
     if 'fileUpload' not in request.FILES:
         return Response({"error": "No file provided."}, status=status.HTTP_400_BAD_REQUEST)
     
@@ -44,23 +36,3 @@ def upload_image(request):
         return Response({"error": "Error processing image from API."}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     return Response(api_response, status=status.HTTP_201_CREATED)
-
-# Create your views here.
-def send_info(request):
-    data = {
-        'message': 'Hello from Django!',
-        'status': 'success',
-        'data': {
-            'info': 'Some important information here.'
-        }
-    }
-    return JsonResponse(data)
-
-@api_view(['POST'])
-def post_message(request):
-    if 'message' in request.data:
-        message = request.data
-        print('Received message:', message)
-        return Response({"message": "Message received."}, status=status.HTTP_201_CREATED)
-   
-    return Response({"error": "No message provided."}, status=status.HTTP_400_BAD_REQUEST)
--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -9,25 +9,22 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from django.db import connections, OperationalError
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&g7j$8k@%#y#!%71(xw3=sy40-r%-ps^_nds9u6+9du+a$!4v9'
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&g7j$8k@%#y#!%71(xw3=sy40-r%-ps^_nds9u6+9du+a$!4v9')
+DEBUG = False
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1').split()
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
@@ -44,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-
 ]
 
 MIDDLEWARE = [
@@ -96,8 +92,16 @@ DATABASES = {
         'NAME': 'DescriptionDB',
         'USER': 'myuser',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': '5432',
+    },
+    'replica': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DescriptionDB',
+        'USER': 'myuser',
+        'PASSWORD': 'postgres',
+        'HOST': 'db-replica',
+        'PORT': '5433',
     },
 }
 
